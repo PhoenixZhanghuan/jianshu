6dc2f7101569fd693b07787b33f67d84dcfa744d
张欢 <zhanghuan@zhanghuandeMacBook-Pro.local>
2020-11-29 17:55:44 +0800

    添加详情页。

--- src/pages/detail/index.js
+++ src/pages/detail/index.js
@@ -1,13 +1,22 @@
-import React, {PureComponent} from 'react'
-
+import React, { PureComponent } from "react";
+import { DetailWrapper, Header, Content } from "./style";
+import { connect } from "react-redux";
 class Detail extends PureComponent {
     render() {
+        const { title, content } = this.props;
+
         return (
-             <div>
-                 Detail~
-             </div>
+            <DetailWrapper>
+                <Header>{title}</Header>
+                <Content dangerouslySetInnerHTML={{ __html: content }} />
+            </DetailWrapper>
         );
     }
 }
 
-export default Detail;
+const mapState = (state) => ({
+    title: state.getIn(["detail", "title"]),
+    content: state.getIn(["detail", "content"]),
+});
+
+export default connect(mapState, null)(Detail);
